b456b31059b88387ac0a8b6438432122f9de41fb
Helton souza silveira <heltonss@dev.local>
2021-02-11 20:27:13 -0300

    add: config para chamada firebase

--- src/App.js
+++ src/App.js
@@ -1,110 +1,104 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-import Api from './Api';
+import Api from "./Api";
 
-import ChatListItem from './components/ChatListItem';
-import ChatIntro from './components/ChatIntro';
-import ChatWindow from './components/ChatWindow';
-import NewChat from './components/NewChat';
-import Login from './components/Login';
+import ChatListItem from "./components/ChatListItem";
+import ChatIntro from "./components/ChatIntro";
+import ChatWindow from "./components/ChatWindow";
+import NewChat from "./components/NewChat";
+import Login from "./components/Login";
 
-import DonutLargeIcon from '@material-ui/icons/DonutLarge';
-import ChatIcon from '@material-ui/icons/Chat';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import SearchIcon from '@material-ui/icons/Search';
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
 
 export default () => {
-
   const [chatlist, setChatList] = useState([]);
   const [activeChat, setActiveChat] = useState({});
-  const [user, setUser] = useState(null
-    //{
-    //id: 'e0kT0gQG7TQt8YQj8fpH5ZU2cwI2',
-    //name: 'Kelly Santos',
-    //avatar: 'https://graph.facebook.com/165759788471062/picture'
-    //}
-  );
+  const [user, setUser] = useState({
+    id: "GAatqwZWIjOL4lyxXEugPXH0rWo2",
+    name: "Helton Souza Silveira",
+    avatar: "https://graph.facebook.com/4996232733782255/picture",
+  });
 
   const [showNewChat, setShowNewChat] = useState(false);
 
-  useEffect(()=>{
-    if(user !== null) {
-        let unsub = Api.onChatList(user.id, setChatList);
-        return unsub;
+  useEffect(() => {
+    if (user !== null) {
+      let unsub = Api.onChatList(user.id, setChatList);
+      return unsub;
     }
   }, [user]);
 
   const handleNewChat = () => {
     setShowNewChat(true);
-  }
+  };
 
   const handleLoginData = async (u) => {
     let newUser = {
-        id: u.uid,
-        name: u.displayName,
-        avatar: u.photoURL
+      id: u.uid,
+      name: u.displayName,
+      avatar: u.photoURL,
     };
-    await Api.addUser(newUser); 
+    await Api.addUser(newUser);
     setUser(newUser);
-  }
+  };
 
-  if(user === null){
-    return (<Login onReceive={handleLoginData} />)
+  if (user === null) {
+    return <Login onReceive={handleLoginData} />;
   }
 
   return (
     <div className="app-window">
-        <div className="sidebar">
-          <NewChat
-            chatlist={chatlist}
-            user={user} 
-            show={showNewChat}
-            setShow={setShowNewChat}
-          />
-          <header>
-            <img className="header--avatar" src={user.avatar}
-             alt="" />
-            <div className="header--buttons">
-              <div className="header--btn">
-                <DonutLargeIcon style={{color: '#919191'}} />
-              </div>
-              <div onClick={handleNewChat} className="header--btn">
-                <ChatIcon style={{color: '#919191'}} />
-              </div>
-              <div className="header--btn">
-                <MoreVertIcon style={{color: '#919191'}} />
-              </div>
+      <div className="sidebar">
+        <NewChat
+          chatlist={chatlist}
+          user={user}
+          show={showNewChat}
+          setShow={setShowNewChat}
+        />
+        <header>
+          <img className="header--avatar" src={user.avatar} alt="" />
+          <div className="header--buttons">
+            <div className="header--btn">
+              <DonutLargeIcon style={{ color: "#919191" }} />
             </div>
-          </header>
-          <div className="search">
-            <div className="search--input">
-                <SearchIcon style={{color: '#919191'}} />
-                <input type="search" placeholder="Procurar ou começar uma nova conversa" />
+            <div onClick={handleNewChat} className="header--btn">
+              <ChatIcon style={{ color: "#919191" }} />
+            </div>
+            <div className="header--btn">
+              <MoreVertIcon style={{ color: "#919191" }} />
             </div>
           </div>
-          <div className="chatlist">
-            {chatlist.map((item, key) => (
-              <ChatListItem 
-                key={key}
-                data={item}
-                active={activeChat.chatId === chatlist[key].chatId}
-                onClick={() => setActiveChat(chatlist[key])}
-              />
-            ))}
-          </div>
-        </div>
-        <div className="contentarea">
-          {activeChat.chatId !== undefined && 
-            <ChatWindow 
-                user={user}
-                data={activeChat}
+        </header>
+        <div className="search">
+          <div className="search--input">
+            <SearchIcon style={{ color: "#919191" }} />
+            <input
+              type="search"
+              placeholder="Procurar ou começar uma nova conversa"
             />
-          }
-          {activeChat.chatId === undefined && 
-            <ChatIntro />
-          }
+          </div>
         </div>
+        <div className="chatlist">
+          {chatlist.map((item, key) => (
+            <ChatListItem
+              key={key}
+              data={item}
+              active={activeChat.chatId === chatlist[key].chatId}
+              onClick={() => setActiveChat(chatlist[key])}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="contentarea">
+        {activeChat.chatId !== undefined && (
+          <ChatWindow user={user} data={activeChat} />
+        )}
+        {activeChat.chatId === undefined && <ChatIntro />}
+      </div>
     </div>
   );
 };
